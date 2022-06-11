a5a96ee4838732e7eb3bfe1c28f459e271785899
Dsprtn <drtsprtn@gmail.com>
2022-06-11 15:53:13 +0200

    initial

---
+++ HelloGL.cpp
@@ -0,0 +1,176 @@
+﻿#include <glad.h>
+#include <glfw3.h>
+#include <iostream>
+#include <chrono>
+#include <math.h>
+#include "HelloGL.h"
+
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core \n"
+"out vec4 FragColor; \n"
+"void main()\n"
+"{\n"
+"FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"}\0";
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+
+int main(int argc, char* argv[])
+{
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+  auto now = std::chrono::high_resolution_clock::now();
+  auto last = std::chrono::high_resolution_clock::now();
+
+  float totalElapsed = 0.0f;
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+  glCompileShader(vertexShader);
+
+
+  CheckShaderCompilationSuccesful(vertexShader);
+
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  CheckShaderCompilationSuccesful(fragmentShader);
+
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  float vertices[] = {
+ -0.5f, -0.5f, 0.0f,
+  0.5f, -0.5f, 0.0f,
+  0.0f,  0.5f, 0.0f
+  };
+
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+
+  glUseProgram(shaderProgram);
+  glBindVertexArray(VAO);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    now = std::chrono::high_resolution_clock::now();
+    const auto elapsed = std::chrono::high_resolution_clock::now() - last;
+    const unsigned long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+    totalElapsed += ((double)microseconds / 1000000);
+    last = now;
+
+    glClearColor(sin(totalElapsed), sin(0), sin(0), 1.0f);
+    std::cout << totalElapsed << std::endl;
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    processInput(window);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+
+  }
+
+  glfwTerminate();
+
+  return 0;
+}
+
+void CheckShaderCompilationSuccesful(unsigned int vertexShader)
+{
+  int  success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+}
+
+void CheckShaderCompilationSuccesful(unsigned int fragmentShader, int& success, unsigned int vertexShader, char  infoLog[512])
+{
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+}
+
---
+++ HelloGL.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+void CheckShaderCompilationSuccesful(unsigned int fragmentShader, int& success, unsigned int vertexShader, char  infoLog[512]);
+
+void CheckShaderCompilationSuccesful(unsigned int vertexShader);
